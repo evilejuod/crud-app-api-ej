8e3c6963ddff7f5401793fbeefd33f9ce3a7778d
Evile <evile.juodziukynaite@gmail.vom>
2021-12-18 21:43:12 +0200

    add user delete & update

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -14,7 +14,12 @@ const getAllUsers = async (req, res) => {
 
 
 const getUserById = async (req, res) => {
-  
+  // const sql = `SELECT * FROM users WHERE id=?`;
+
+
+
+
+
 }
 
 //POST/api/users
@@ -46,17 +51,54 @@ const createUser = async (req, res) => {
   res.status(500).json({ error: 'something went wrong' })
 }
 
+
+//DELETE/api/users/:id
 const deleteUser = async (req, res) => {
-  
+  const { id } = req.params
+  const sql = ` DELETE FROM users WHERE id = ?`;
+
+  const dbResult = await dbAction(sql, [id])
+
+  if (dbResult === false) return dbFail(res, 'error deleting user');
+
+  if (dbResult.affectedRows === 1) {
+    dbSuccess(res, 'user deleted')
+  }
+
+  res.status(500).json({ error: 'something went wrong' })
+
 }
 
-
+//PUT/api/users/:id
 const updateUser = async (req, res) => {
 
+  const userToUpdate = {
+    name: req.body.name,
+    age: req.body.age,
+    email: req.body.email,
+    password: hashValue(req.body.password),
+    id: req.params.id
+  };
+
+  const sql = ` 
+    UPDATE users SET name=?, age=?, email=?, password=?
+    WHERE id = ?
+    `;
+
+  const dbResult = await dbAction(sql, Object.values(userToUpdate));
+
+  if (dbResult === false) {
+    return res.status(500).json({ error: 'something went wrong' });
+  }
+
+  if (dbResult.affectedRows === 1) {
+    return res.json({ msg: 'user has been updated', user: userToUpdate.email });
+  }
+
+  res.status(500).json({ error: 'something went wrong' })
   
 
 }
-
 
 
 module.exports = {
